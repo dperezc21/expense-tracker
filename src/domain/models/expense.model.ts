ef35e9d48226a60accd6fa3d394775aefde0ee4f
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-02-01 00:43:49 -0500

    feat: define expense model

---
+++ src/domain/models/expense.model.ts
@@ -0,0 +1,9 @@
+import {Expense} from "../interfaces/Expense";
+
+
+export class ExpenseModel implements Expense {
+    category!: string;
+    date!: Date;
+    description!: string;
+    id!: number;
+}
